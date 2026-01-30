710fb01ab99c3df0412c382c3044c72c53d8d48c
Johny Villegas <johny.villegas.dev@gmail.com>
2026-01-30 19:38:29 +0100

    mas ajustes en firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,20 @@
+import dotenv from "dotenv";
 import admin from "firebase-admin";
-import path from "path";
+dotenv.config();
 
-const filePath = path.resolve(__dirname, "..", "..", "serviceAccountKey.json");
-const serviceAccount = require(filePath);
+let envVar = process.env.FIREBASE_SERVICE_ACCOUNT;
+if (!envVar) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable is not set");
+}
+
+let serviceAccount: any;
+try {
+    serviceAccount = JSON.parse(envVar);
+} catch (err: any) {
+    throw new Error(
+        "Invalid JSON in FIREBASE_SERVICE_ACCOUNT environment variable",
+    );
+}
 
 let firebaseApp: any;
 
@@ -11,7 +23,8 @@ try {
     if (existingApps.length === 0) {
         firebaseApp = admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
-            projectId: serviceAccount.project_id,
+            projectId:
+                serviceAccount.project_id || process.env.FIREBASE_PROJECT_ID,
         });
     } else {
         firebaseApp = existingApps[0];
@@ -20,7 +33,6 @@ try {
     throw error;
 }
 
-// Export services using the initialized app instance
 export const firebaseDB = admin.firestore(firebaseApp);
 export const firebaseAuth = admin.auth(firebaseApp);
 export default admin;
